3754159bb81e9a00586e37ad23c034facb00208d
Amandella <matthewdipillo@gmail.com>
2024-01-19 07:48:39 -0500

    More work on the wall ghost.

--- Scenes/Ghosts/Attacks/goo_puddle.gd
+++ Scenes/Ghosts/Attacks/goo_puddle.gd
@@ -2,9 +2,31 @@ extends Area2D
 
 @onready var goo_puddle = $"."
 @onready var puddle_timer = $PuddleTimer
+@onready var hurt_box = $HurtBox
+
+@export var damagePerTick: int
+@export var team: Globals.Teams = Globals.Teams.ENEMIES
+
+var in_goo: bool = false
+var player = null
 
 func _ready():
 	puddle_timer.start(6)
+	hurt_box.initialize_team(team)
+	hurt_box.set_damage(damagePerTick)
 
 func _on_puddle_timer_timeout():
 	goo_puddle.queue_free()
+
+func _on_damage_timer_timeout():
+	if in_goo:
+		player.take_damage(hurt_box.damage)
+
+func _on_hurt_box_area_entered(area):
+	if area.team != team and area.has_method("take_damage"):
+		in_goo = true
+		player = area
+		
+func _on_hurt_box_area_exited(area):
+	if area.team != team and area.has_method("take_damage"):
+		in_goo = false
--- Scenes/Ghosts/Attacks/gooball.gd
+++ Scenes/Ghosts/Attacks/gooball.gd
@@ -5,10 +5,13 @@ class_name GooBall
 signal createGooPuddle()
 
 const GOO_PUDDLE = preload("res://Scenes/Ghosts/Attacks/goo_puddle.tscn")
+const IMPACT_PARTICLE = preload("res://Scenes/Ghosts/Attacks/impact_particle.tscn")
 
 @onready var hurt_box = $HurtBox
 @onready var goo_ball = $"."
 @onready var explode_timer = $ExplodeTimer
+@onready var sprite_2d = $Sprite2D
+@onready var shadow = $Shadow
 
 @export var team: Globals.Teams = Globals.Teams.ENEMIES
 @export var speed: int
@@ -21,10 +24,11 @@ func _ready():
 	hurt_box.set_damage(damage)
 	direction = global_position.direction_to(Globals.currentPlayer.global_position)
 	velocity = speed * direction
-	explode_timer.start(3)
+	explode_timer.start()
+	sprite_2d.rotate(randf_range(0,360))
 	
 func _physics_process(_delta):
-	rotate(get_angle_to(Globals.currentPlayer.global_position))
+	
 	move_and_slide()
 
 func _on_explode_timer_timeout():
@@ -33,4 +37,8 @@ func _on_explode_timer_timeout():
 
 func _on_hurt_box_area_entered(_area):
 	createGooPuddle.emit()
+	var impact = IMPACT_PARTICLE.instantiate()
+	impact.position = global_position
+	impact.emitting = true
+	get_tree().current_scene.add_child(impact)
 	goo_ball.queue_free()
